fb79b7327a46186051afb9e86b12a0b21a375ccf
nisthaKumar <nistha.may2994@gmail.com>
2023-07-31 00:43:37 +0530

    Taking user input for the api

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template
+import pandas as pd
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("test.html")
+    return render_template("home.html")
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature =23
+    filename = "/Users/nistha/Documents/Python/Weather-Data-API/data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows = 20, parse_dates=["    DATE"])
+    #temperature =23
+    temperature = df.loc[df['    DATE']== date]['   TG'].squeeze()/10
     return {"station" : station,
             "date" : date,
             "temperatue" : temperature}
 
-@app.route("/Contact/")
-def contact_me():
-    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
